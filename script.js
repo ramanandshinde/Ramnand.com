1f0390513da2d23603aaef02dcd07940c0e97b17
ramanandshinde <160994126+ramanandshinde@users.noreply.github.com>
2024-06-25 20:52:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+var leo = new Swiper(".myone", {
+    slidesPerView:7,
+    spaceBetween: 30,
+    freeMode: true,
+     autoplay:true,
+     loop:true,
+   
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        320 :{
+          slidesPerView: 3,
+          spaceBetween:10,
+        },
+
+        375 :{
+          slidesPerView: 3,
+          spaceBetween:10,
+        },
+        425 :{
+          slidesPerView: 1,
+          spaceBetween:10,
+        },
+        768 :{
+          slidesPerView: 2,
+          spaceBetween:10,
+        },
+        1024:{
+          slidesPerView: 9,
+          spaceBetween:10,
+        }
+        
+    }
+  });
+
+  
+
+  var low = new Swiper(".mylow", {
+    slidesPerView:4,
+    spaceBetween: 30,
+    freeMode: true,
+     autoplay:true,
+     loop:true,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+   
+     breakpoints:{
+      320 :{
+        slidesPerView: 1,
+        spaceBetween:10,
+      },
+
+      375 :{
+        slidesPerView: 1.2,
+        spaceBetween:10,
+      },
+      425 :{
+        slidesPerView: 1,
+        spaceBetween:10,
+      },
+      768 :{
+        slidesPerView: 2,
+        spaceBetween:10,
+      },
+      1024:{
+        slidesPerView: 4,
+        spaceBetween:10,
+      }
+      
+  }
+  });
+
+  var live = new Swiper(".one", {
+    slidesPerView:4,
+    spaceBetween: 30,
+    freeMode: true,
+    //  autoplay:true,
+     loop:true,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     breakpoints:{
+      320 :{
+        slidesPerView: 1,
+        spaceBetween:10,
+      },
+
+      375 :{
+        slidesPerView: 1.2,
+        spaceBetween:10,
+      },
+      425 :{
+        slidesPerView: 1,
+        spaceBetween:10,
+      },
+      768 :{
+        slidesPerView: 2,
+        spaceBetween:10,
+      },
+      1024:{
+        slidesPerView: 4,
+        spaceBetween:10,
+      }
+      
+  }
+  });
+
+  var apk = new Swiper(".myapk", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    freeMode: true,
+     autoplay:true,
+     loop:true,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+   
+      breakpoints:{
+        320 :{
+          slidesPerView: 1,
+          spaceBetween:10,
+        },
+  
+        375 :{
+          slidesPerView: 1,
+          spaceBetween:10,
+        },
+        425 :{
+          slidesPerView: 4,
+          spaceBetween:10,
+        },
+        768 :{
+          slidesPerView: 2,
+          spaceBetween:10,
+        },
+        1024:{
+          slidesPerView: 4,
+          spaceBetween:15,
+        },
+        
+      
+    },
+  });
+
+
+  var boat = new Swiper(".myboat", {
+    slidesPerView:4,
+    spaceBetween: 30,
+    freeMode: true,
+    // autoplay:true,
+     loop:true,
+    //  mousewheel:true,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     
+     breakpoints:{
+      320 :{
+        slidesPerView: 1.5,
+        spaceBetween:10,
+      },
+
+      375 :{
+        slidesPerView: 1.5,
+        spaceBetween:10,
+      },
+      425 :{
+        slidesPerView: 4,
+        spaceBetween:10,
+      },
+      768 :{
+        slidesPerView: 2,
+        spaceBetween:10,
+      },
+      1024:{
+        slidesPerView: 4,
+        spaceBetween:15,
+      },
+      
+    
+  },
+  });
+  
+
+  var loop = new Swiper(".myloop", {
+    slidesPerView:3,
+    spaceBetween: 30,
+    freeMode: true,
+    // autoplay:true,
+     loop:true,
+    //  mousewheel:true,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     
+     breakpoints:{
+      320 :{
+        slidesPerView: 1.5,
+        spaceBetween:10,
+      },
+
+      375 :{
+        slidesPerView: 1.5,
+        spaceBetween:10,
+      },
+      425 :{
+        slidesPerView: 4,
+        spaceBetween:10,
+      },
+      768 :{
+        slidesPerView: 2,
+        spaceBetween:10,
+      },
+      1024:{
+        slidesPerView: 4,
+        spaceBetween:15,
+      },
+      
+    
+  },
+  });
+  
